39314f229dbe63c505c9ef8ef997078118148346
Vincenzo Farella <Enzino_2009@hotmail.it>
2022-11-02 10:38:37 +0100

    Update Max Citizens

--- Eurozone/Country/City.cs
+++ Eurozone/Country/City.cs
@@ -6,21 +6,28 @@ using System.Threading.Tasks;
 
 namespace Eurozone.Country
 {
+
     public class City
     {
         private string nome;
         public List<Citizen> citizens;
         public string myCountry;
+        public int maxCitizens;
+        private Random _random = new Random();
+        private string[] names = { "Luca", "Claudio", "Armando", "Francesco", "Andrea" };
+        private string[] surnames = { "Bianchi", "Rossi", "Colle", "Russo", "Soleri" };
 
-        public City(string _nome, string _country)
+        public City(string _nome, string _country, int _maxCitizens)
         {
             nome = _nome;
             myCountry = _country;
+            maxCitizens = _maxCitizens;
+            citizens = new List<Citizen>();
         }
 
         public void addCitizen(Citizen newCitizen)
         {
-            if (citizens.IndexOf(newCitizen) == -1)
+            if (citizens.IndexOf(newCitizen) == -1 && citizens.Count < maxCitizens)
                 citizens.Add(newCitizen);
         }
 
@@ -30,6 +37,50 @@ namespace Eurozone.Country
                 citizens.Remove(newCitizen);
 
             newCity.addCitizen(newCitizen);
+        }
+
+        public void manualAddCitizen()
+        {
+            if (citizens.Count < maxCitizens)
+            {
+                string newnome = "", newcognome = "";
+                Console.WriteLine("Città: " + nome + "\n");
+                Console.WriteLine("* Nome: ");
+                newnome = Console.ReadLine();
+                Console.WriteLine("* Cognome: ");
+                newcognome = Console.ReadLine();
+                citizens.Add(new Citizen(newnome, newcognome, nome));
+            }
+            else Console.WriteLine("Città " + nome + " al completo. ");
+        }
+
+        public void randomAddCitizens(int _random_generation)
+        {
+            if(_random_generation > (maxCitizens - citizens.Count))
+                _random_generation = (maxCitizens - citizens.Count);
+
+            for (int i = 0; i < _random_generation; i++)
+            {
+                citizens.Add(new Citizen(genName(), genSurname(), nome));
+
+            }
+        }
+
+        public void printCitizens()
+        {
+            Console.WriteLine("Città: " + nome + "\n");
+            for (int i = 0; i < citizens.Count; i++)
+                Console.WriteLine("> " + citizens[i].nome + " " + citizens[i].cognome);
+        }
+
+        public string genName()
+        {
+            return names[_random.Next(names.Length)];
+        }
+
+        public string genSurname()
+        {
+            return surnames[_random.Next(surnames.Length)];
         }
 
 
--- Eurozone/Program.cs
+++ Eurozone/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Eurozone.Country;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,25 +11,22 @@ namespace Eurozone
     {
         static void Main(string[] args)
         {
-            /*
-            25.10.2022
-
             CountryEU_ONU italiaCountry = new CountryEU_ONU(60000, 324000, "Europa", "Italia", "Costituzione Italiana", "VerdeBiancoRosso", "Euro", "Italiano", 100, false);
-            CountryEU_ONU franciaCountry = new CountryEU_ONU(67000, 675000, "Europa", "Francia", "Costituzione Francese", "BluBiancoRosso", "Euro", "Francese", 150, false);
-            CountryEU_ONU germaniaCountry = new CountryEU_ONU(83000, 357000, "Europa", "Germania", "Legge Fondamentale Tedesca", "NeroRossoGiallo", "Euro", "Tedesco", 200, false);
+            //CountryEU_ONU franciaCountry = new CountryEU_ONU(67000, 675000, "Europa", "Francia", "Costituzione Francese", "BluBiancoRosso", "Euro", "Francese", 150, false);
+            //CountryEU_ONU germaniaCountry = new CountryEU_ONU(83000, 357000, "Europa", "Germania", "Legge Fondamentale Tedesca", "NeroRossoGiallo", "Euro", "Tedesco", 200, false);
+            //CountryONU cinaCountry = new CountryONU(1400000, 9706000, "Asia", "Cina", "Costituzione della Repubblica Popolare Cinese", "RossoGiallo", "Renminbi", "Cinese Mandarino", 30, true);
 
-            CountryONU cinaCountry = new CountryONU(1400000, 9706000, "Asia", "Cina", "Costituzione della Repubblica Popolare Cinese", "RossoGiallo", "Renminbi", "Cinese Mandarino", 30, true);
+            City romeCity = new City("Rome", "Italy", 20);
+            City milanCity = new City("Milan", "Italy", 10);
 
-            Console.WriteLine("Italia");
-            italiaCountry.monetaUnica();
-            italiaCountry.PenaMorte();
-            italiaCountry.calculateSpread();
-            Console.WriteLine("*\n");
+            romeCity.randomAddCitizens(8);
+            milanCity.randomAddCitizens(4);
 
-            Console.WriteLine("Cina");
-            cinaCountry.PenaMorte();
-            Console.WriteLine("*\n");
-            */
+            milanCity.manualAddCitizen();
+
+            Console.Clear();
+
+            milanCity.printCitizens();
 
             Console.ReadKey();
         }
