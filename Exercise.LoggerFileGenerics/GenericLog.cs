3183bb02e308561890855b9e43caa66bf4dfd08c
Vincenzo Farella <Enzino_2009@hotmail.it>
2022-11-17 10:32:20 +0100

    Update Logger Generics

---
+++ Exercise.LoggerFileGenerics/GenericLog.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise.LoggerFileGenerics
+{
+    public static class GenericLog
+    {
+        public static void LogToFile<T>(List<T> data, string path) where T : class
+        {
+            StringBuilder line = new StringBuilder();
+
+            if(data == null || data.Count == 0)
+                throw new ArgumentException("data", "empty list or null");
+
+            foreach(var item in data)
+            {
+                var element = item.GetType().GetProperties();
+                Console.WriteLine(element.Length);
+                foreach (var attr in element)
+                {
+                    line.Append(attr.Name);
+                    Console.WriteLine(attr.Name);
+                }
+            }
+
+            File.AppendAllText(path, line.ToString());
+        }
+
+        public static void LogToFile<T>(T data, string path) where T : class
+        {
+            if (data == null)
+                throw new ArgumentException("data", "empty item or null");
+
+            StringBuilder line = new StringBuilder();
+            foreach (var attr in data.GetType().GetProperties())
+            {
+                line.Append(attr.Name);
+                Console.WriteLine(attr.Name);
+            }
+
+            File.AppendAllText(path, line.ToString());
+        }
+    }
+}
---
+++ Exercise.LoggerFileGenerics/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Exercise.LoggerFileGenerics
+{
+    internal class Program
+    {
+        static string filepath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "GenericLog.txt");
+        static void Main(string[] args)
+        {
+            List<Point> points = new List<Point>();
+            points.Add(new Point(3, 3));
+            points.Add(new Point(5, 5));
+
+            Line line = new Line(new Point(3, 3), new Point(5, 5));
+
+            Console.WriteLine("Test");
+
+            GenericLog.LogToFile(points, filepath);
+            GenericLog.LogToFile(line, filepath);
+        }
+
+        public class Point {
+            public int x;
+            public int y;
+        
+            public int Square()
+            {
+                return x ^ 2 * y ^ 2;
+            }
+
+            public Point(int _x, int _y)
+            {
+                x = _x;
+                y = _y;
+            }
+        }
+
+        public class Line
+        {
+            public Point p1;
+            public Point p2;
+
+            public Line(Point _p1, Point _p2)
+            {
+                p1 = _p1;
+                p2 = _p2;
+            }
+        }
+    }
+}
