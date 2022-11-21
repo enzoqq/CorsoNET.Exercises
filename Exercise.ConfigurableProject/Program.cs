c882066a825f9b64bc19f2468edbb7a67a404039
Vincenzo Farella <Enzino_2009@hotmail.it>
2022-11-21 10:13:31 +0100

    Configurable JSON Project

---
+++ Exercise.ConfigurableProject/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+
+namespace Exercise.ConfigurableProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IConfiguration config = new ConfigurationBuilder().AddJsonFile("appconfig.json").Build();
+            Settings settings = config.GetRequiredSection("settings").Get<Settings>();
+            Console.WriteLine(settings.Value1 + " " + settings.Value2);
+        }
+    }
+
+    public class Settings
+    {
+        public string Value1 { get; set; }
+        public string Value2 { get; set; }
+    }
+}
